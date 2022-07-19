672dec84d685363179d9b05d81ac5c43ccb4f3bb
Francesco <francescoEngim>
2022-07-19 13:36:37 +0200

    first commit

---
+++ src/main/java/cicciofr/barinord/BariNordApplication.java
@@ -0,0 +1,13 @@
+package cicciofr.barinord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BariNordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BariNordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cicciofr/barinord/BariNordApplicationTests.java
@@ -0,0 +1,13 @@
+package cicciofr.barinord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BariNordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
