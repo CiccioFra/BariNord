a209ee6346f0e477f13d6a69a64bbf565f957065
Francesco <francescoEngim>
2022-07-19 15:17:29 +0200

    Credenziali connessione al DB

---
+++ src/main/java/cicciofr/barinord/costanti/DBConfig.java
@@ -0,0 +1,7 @@
+package cicciofr.barinord.costanti;
+
+public class DBConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/barinord";
+    public static final String USER = "BariNord";
+    public static final String PW = "password";
+}
